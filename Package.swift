e84ff053860cb1f9a39aa9a3aa35487bd85bb80e
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2025-07-11 00:47:55 +0200

    Update Package.swift for 6.9.10 release

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "Oniguruma",
             url: "https://github.com/krzyzanowskim/oniguruma/releases/download/6.9.10/Oniguruma.xcframework.zip",
-            checksum: "d68ae7e92d33fa7d0ae975b860b9122e7265c7f46095114a89dfd99fbf85222a"
+            checksum: "d49ca9245b4915a8d5a9d76f3aa67379d0cad35267c9f7b7aadadcdb5581f96c"
         )
     ]
 )
